5a6836a9bd7fc56ac3f863811d14830ddce34a62
karim-kessentini <karim.kessentini1@gmail.com>
2025-08-28 15:59:46 -0700

    add properties grid react island

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind()],
+  integrations: [icon(), tailwind(), react()],
   site: "https://agency-template.vbartalis.dev",
-  base: "",
+  base: ""
 });
