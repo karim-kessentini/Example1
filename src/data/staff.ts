93cea9ae1ce326d99f25cb2b0b11ef2c4bec282b
karim-kessentini <karim.kessentini1@gmail.com>
2025-09-05 14:59:33 -0700

    Add staff section with agent cards and images; update index page to include Staff component

---
+++ src/data/staff.ts
@@ -0,0 +1,27 @@
+import michaelRutter from '../assets/images/staff/michael-rutter.webp';
+import jimDaniels from '../assets/images/staff/jim-daniels.webp';
+import mayaPatel from '../assets/images/staff/maya-patel.webp';
+import samSmith from '../assets/images/staff/sam-smith.webp';
+
+export const staff = [
+    {
+        "name": "Michael Rutter",
+        "role": "buying agent",
+        "photo": michaelRutter
+    },
+    {
+        "name": "Jim Daniels",
+        "role": "selling agent",
+        "photo": jimDaniels
+    },
+    {
+        "name": "Maya Patel",
+        "role": "sales executive",
+        "photo": mayaPatel
+    },
+    {
+        "name": "Sam Smith",
+        "role": "director",
+        "photo": samSmith
+    },
+]
